91c337f5b0bcd14b23cb7947fa8f114de44aff4c
Danielle <mdkspencer@gmail.com>
2021-06-30 22:20:04 +0200

    add date and time to header and remove manual built in commands

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <header class="flex bg-bg">
       <div
         class="
+          grid grid-cols-3
           w-full
           overflow-hidden
           max-w-screen-lg
@@ -13,9 +14,10 @@
           font-bold
           text-lg
         "
-        id="title"
       >
-        PORTFOLIO
+        <div>{{ currentDate() }}</div>
+        <div id="title">PORTFOLIO</div>
+        <div>{{ timeNow }}</div>
       </div>
     </header>
 
@@ -25,7 +27,6 @@
         v-on:click="focusTerminal"
       >
         <vue-command
-          :built-in="builtIn"
           :history.sync="history"
           :is-in-progress="isInProgress"
           :pointer="pointer"
@@ -49,7 +50,7 @@
         id="footer"
       >
         <p class="text-bg text-xl ml-1 mr-6 px-1 invisible" id="placeholder">
-          <button class="text-bg3 text-xl"></button>
+          <button class="text-bg text-xl"></button>
           CLOSE
         </p>
       </div>
@@ -59,13 +60,13 @@
 
 <script>
 import VueCommand, { createStdout, createDummyStdout } from "vue-command";
+import moment from "moment";
 import "./main.css";
 import Education from "./components/Education.vue";
 import Experience from "./components/Experience.vue";
 import Skill from "./components/Skill.vue";
 import User from "./components/User.vue";
 import Help from "./components/Help.vue";
-import axios from "axios";
 
 export default {
   name: "App",
@@ -82,15 +83,10 @@ export default {
     commands: {
       something: () => createStdout("Haha... very funny"),
       education: () => Education,
-      EDUCATION: () => Education,
       experience: () => Experience,
-      EXPERIENCE: () => Experience,
       skills: () => Skill,
-      SKILLS: () => Skill,
       users: () => User,
-      USERS: () => User,
       help: () => Help,
-      HELP: () => Help,
     },
     hideBar: true,
     hideTitle: true,
@@ -99,70 +95,34 @@ export default {
     api: false,
     endpoint: "http://localhost:8081",
     user: false,
-    apikey: "8b0973a152c2b5a2caf146f34222d42c3955234d",
-    data: [],
-    answer: null,
+    timeNow: "",
   }),
   methods: {
-    connect(question) {
-      this.api = axios.create({
-        baseURL: this.endpoint,
-        headers: { Authorization: "Token " + this.apikey },
-      });
-
-      this.api
-        .post("question", {
-          question: question,
-        })
-        .then((response) => {
-          this.data = response.data;
-          console.log(this.data.answer);
-
-          this.executed.delete(question);
-          this.executed.add(question);
-          this.pointer += 1;
-          this.history.push(createStdout(this.data.answer));
-
-          this.isInProgress = false;
-        })
-        .catch((error) => {
-          if (error && error.response && error.response.status == 401) {
-            alert("Your API Key seems invalid, please try a different one");
-          } else {
-            console.log(error);
-            alert("There was an error while connecting to the API");
-          }
-        });
-    },
-
     focusTerminal() {
       this.$refs.terminal.focus();
+    },
+    currentDate() {
+      return moment().format("D MMMM YYYY");
+    },
+    currentTime() {
+      this.timeNow = moment().format("h:mm:ss a");
     },
   },
   mounted() {
     // this.$refs.terminal.focus();
+    this.currentTime();
+    setInterval(this.currentTime, 1000)
+
   },
   created() {
     var message =
       "PORTFOLIO version 1.0.0<br> ©" +
       new Date().getFullYear() +
-      " Danielle Spencer<br> Current date is " +
-      new Date().toLocaleDateString() +
+      " Danielle Spencer<br> " +
       "<br> Welcome to my portfolio<br><br>" +
       "To get started, try typing in 'help'";
 
     this.history.push(createStdout(message));
-
-    this.builtIn = (stdin) => {
-      this.isInProgress = true;
-
-      if (stdin.toLowerCase() == "download") {
-        this.history.push(createStdout("CV opened in new tab"));
-        window.open("/DSpencer_CV.pdf", "_blank");
-      } else {
-        this.connect(stdin);
-      }
-    };
 
     this.commands.clear = () => {
       this.history = [];
