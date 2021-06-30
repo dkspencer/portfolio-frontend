bb5f063f19bfc6c0c2ac230cbf26ef22248f271b
Danielle <mdkspencer@gmail.com>
2021-06-30 22:23:10 +0200

    change colorscheme to grey/mustard

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
+    theme: {
+        fontFamily: {
+            'sans': ['Courier New', 'monospace'],
+            'serif': ['Courier New', 'monospace'],
+            'mono': ['Courier New', 'monospace']
+        },
+        extend: {
+            colors: {
+                bg: '#3c3836',
+                fg: '#deb15a'
+            },
+        }
+    },
+    variants: {},
+    plugins: [],
+}
